df18d557d8ca988eab5efba56a309ae019e1a351
Szymon Bogus <bogusszymon2000@gmail.com>
2024-08-01 10:19:18 +0200

    Added Python UDP server for testing TelemetrySender

---
+++ DronePositioningWinAppBackend/DronePositioningWinAppBackend/tests/testTelemetrySender.py
@@ -0,0 +1,28 @@
+"""
+The following code implements UDP server which receives data
+from remote client. This script aims to test TelemetrySender implmenetation.
+"""
+import socket
+
+
+UDP_IP = "0.0.0.0"  
+UDP_PORT = 1337     
+
+if __name__ == "__main__":
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  t
+    sock.bind((UDP_IP, UDP_PORT))
+
+    print(f"Telemetry receiver listening on {UDP_IP}:{UDP_PORT}")
+
+    while True:
+        data, addr = sock.recvfrom(1024)  # Buffer size of 1024 bytes
+        telemetry_str = data.decode('utf-8')
+
+        # Parse the telemetry string
+        try:
+            telemetry_values = [float(x) for x in telemetry_str.split()] 
+            print(f"Received telemetry: {telemetry_values} from {addr}")
+
+        except ValueError:
+            print(f"Invalid telemetry format: {telemetry_str}")
